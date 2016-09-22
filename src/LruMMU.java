eb34d2a0ed7a3e8093e471174ec3010e85cf77ea
Squid <clinton.page123@gmail.com>
2016-09-22 22:29:17 +0930

    finished LRU and attempted ESC

--- src/LruMMU.java
+++ src/LruMMU.java
@@ -1,48 +1,102 @@
+import java.util.Vector;
+
 /**
 * MMU using least recently used replacement strategy
 * Need to keep track of reference order 
 */
 
 public class LruMMU implements MMU {
-
-    
+	
+	private Vector<Page> pageQueue;
+    private int frameWindowCapacity = 0;
+    private Page lastVictim = null;
+	
+	//constructor
     public LruMMU(int frames) {
-	 // each page has a resident and a modified bit. 
-        //to do
+    	//make a vector of size "frames"
+    	this.pageQueue = new Vector<Page>(frames);
+    	this.frameWindowCapacity = frames;
     }
     
+    //purpose: this method stores whenever a page is read from
     public void readMemory(int page_number) {
-        //this method updates the reference status of a resident page
+    	//record that the page has been read
+    	int index = this.checkInMemory(page_number);
+    	if(index == -1) {
+    		System.out.println("failed to write to a page");
+    		return;
+    	}
+    	Page readPage = pageQueue.get(index);
+    	readPage.referenceBit = true;
+    	MovePageToBottom(index);
     }
     
+    //purpose: this method stores whenever a page is written to
     public void writeMemory(int page_number) {
-        //this method updates the reference status of a resident page and
-	// records it has been modified
-    
+    	//record that the page has been modified
+    	int index = this.checkInMemory(page_number);
+    	if(index == -1) {
+    		System.out.println("failed to write to a page");
+    		return;
+    	}
+    	Page writePage = pageQueue.get(index);
+    	writePage.modifyBit = true;
+    	writePage.referenceBit = true;
+    	MovePageToBottom(index);
     }
-
+    
+    //purpose: moves the page to the bottom of the queue
+    //input: the index of the page to move to the bottom
+    private void MovePageToBottom(int pageIndex) {
+    	Page tempPage = pageQueue.remove(pageIndex);
+    	pageQueue.add(tempPage);
+    }
+    
+    //purpose: check if the page being requested is already loaded in RAM
+    //return: -1 if not currently in RAM. otherwise return the frame number
     public int checkInMemory(int page_number) {
-        // check if a page is resident
-	// returns its location (frame number) or -1 if not resident
+    	//look in queue for page number
+    	for(int i = 0; i < this.pageQueue.size(); ++i ) {
+    		if(this.pageQueue.get(i).pageNo == page_number) {
+    			return i;
+    		}
+    	}
+    	//couldn't find page number
         return -1;
     }
     
+    //purpose: write page to an avaliable frame
+    //note: this method is only called if the simulation knows there is a free frame avaliable.
     public void allocateFrame(int page_number) {
-        // it allocate a page into a free frame
-	// to do
+    	//put at back of queue
+    	Page newPage = new Page(page_number);
+    	
+    	//check size before inserting
+    	if(this.pageQueue.size() >= frameWindowCapacity) {
+    		//throw error
+    		System.out.println("error: hit frame window capacity");
+    		return;
+    	}
+    	pageQueue.add(newPage);
     }
     
-    
+    //purpose: despite the shit name. swaps a frame from the page queue with the page "page_number"
+    //inputs: page_number = the new page that needs to be inserted into the frame window.
+    //return: the page number that was replaced to make room for the new page.
     public int selectVictim(int page_number) {
-        //it select the victim, allocates the new page into the selected frame 
-	//and returns the number of the page replaced
-        return 1;
+    	//the victim page is the first page on the queue
+    	lastVictim = pageQueue.remove(0);
+    	
+    	//insert the new page onto the frame queue
+    	Page newPage = new Page(page_number);
+    	pageQueue.add(newPage);
+    	
+        return lastVictim.pageNo;
     }
     
+    //purpose: tells simulator if last page was modified
+    //returns: true if page was modified and false otherwise.
     public boolean  lastVictimStatus( ) {
-	// it returns true if the last victim was a modified page
-	// false otherwise
-        //todo
-        return false;
+    	return lastVictim.modifyBit;
     }
 }
--- src/Memsim.java
+++ src/Memsim.java
@@ -4,13 +4,16 @@ import java.io.FileReader;
 /*
 run program like this:
 javac *.java
-java Memsim ../testData/sample.trace 4 rand debug
+java Memsim ../testData/trace1 4 lru debug
 */
 
 public class Memsim {
 	private static int page_offset = 12; // page is 2^12 = 4096 bytes
-	private static int frames;
+	private static int frames = 4; //frame window
 	private static boolean debug = false;
+	private static int numEvents = 0; //number of events
+	private static int numDiskReads = 0;
+	private static int numDiskWrites = 0;
 
     private static BufferedReader inputFile = null;
     private static MMU mmu = null;
@@ -19,86 +22,8 @@ public class Memsim {
 	//entry point
     public static void main(String[] args) {
 		ReadParameters(args);
-		
-        
-    /* Process the traces from the file */
-    String traceLine; //current line
-    String[] traceCmd; //parse line
-    long logical_address;
-    int page_number, frame_number;
-	int victim_page;
-	boolean is_modified;
-	int allocated = 0;
-    int no_events = 0;
-	int disk_writes = 0, disk_reads = 0;
-        
-    try {
-        traceLine = inputFile.readLine();
-		
-		//for every job
-        while (traceLine != null) {
-            traceCmd = traceLine.split(" ");
-                
-            //convert from hexadecimal address from file, to appropriate page number
-            logical_address = Long.parseLong(traceCmd[0],16);
-                page_number = (int) (logical_address >>> page_offset);
-		frame_number = mmu.checkInMemory(page_number);
-                
-		if ( frame_number == -1) {
-                // page fault 
-		disk_reads++;
-		if (debug) 
-                    System.out.println("Page fault   " + page_number);
-		if (allocated < frames ) {
-			mmu.allocateFrame(page_number);
-			allocated++;
-		}
-		else { 
-			
-			victim_page = mmu.selectVictim(page_number);
-			is_modified = mmu.lastVictimStatus( );
-		        if (is_modified){
-				disk_writes++;
-				if (debug) System.out.println("Disk write  " + victim_page);
-			}
-			else  if (debug) System.out.println("Discard      " + victim_page);
-		}
-		}
-
-                //process read or write
-                if (traceCmd[1].equals("R")){
-                    mmu.readMemory(page_number);
-		    if (debug) System.out.println("reading      " + page_number);
-		}
-                else if (traceCmd[1].equals("W")){
-                    mmu.writeMemory(page_number);
-		    if (debug) System.out.println("writting     " + page_number);
-		}
-                else {
-                    System.out.println("Badly formatted file. Error on line " + (no_events+1));
-                    System.exit(-1);
-                }
-                
-                no_events++;
-                traceLine = inputFile.readLine();
-            }
-        }
-        catch (java.io.IOException e) {
-            System.out.println("Error reading inputFile file");
-            System.exit(-1);
-        }
-		catch (NumberFormatException e) {
-			System.out.println("Memory address strange on line " + (no_events+1));
-            System.exit(-1);
-		}
-        
-        /* Print results */
-        System.out.println("total memory frames:  " + frames);
-        System.out.println("events in trace:      " + no_events);
-        System.out.println("total disk reads:     " + disk_reads );
-        System.out.println("total disk writes:    " + disk_writes);
-	java.text.DecimalFormat f = new java.text.DecimalFormat("0.0000");
-	System.out.println("page fault rate: " + f.format(((double)disk_reads)/no_events));
+		DoSimulate();
+		DisplayResults();
     }
    
    
@@ -145,5 +70,92 @@ public class Memsim {
             System.exit(-1);
         }
 	}
+   	
+   	private static void DoSimulate() {
+		int numAllocated = 0; //the number of pages currently on the frameWindow
+		try {
+			String traceLine = inputFile.readLine();
+		
+			//for every job
+			while (traceLine != null) {
+				//parse line
+				String[] traceCmd = traceLine.split(" ");
+                
+				//convert from hexadecimal address from file, to appropriate page number
+				long logical_address = Long.parseLong(traceCmd[0],16);
+				//work out the reference number by deviding by the page size
+				int page_number = (int) (logical_address >>> page_offset);
+				//check if frame is in memory
+				int frame_number = mmu.checkInMemory(page_number);
+            
+				//frame not in RAM
+				if ( frame_number == -1) {
+					// page fault 
+					numDiskReads++;
+					if (debug) { 
+						System.out.println("Page fault   " + page_number);
+					}
+            	
+					if (numAllocated < frames ) {
+						//there is space on frame window
+						mmu.allocateFrame(page_number);
+						numAllocated++;
+					} else { 
+						//need to replace a frame
+						int victim_page = mmu.selectVictim(page_number);
+						boolean is_modified = mmu.lastVictimStatus( );
+						if (is_modified){
+							numDiskWrites++;
+							if (debug) {
+								System.out.println("Disk write  " + victim_page);
+							}
+            		
+						} else {
+							if (debug) {
+								System.out.println("Discard      " + victim_page);
+							}	
+						}
+					}
+				}
+
+				//process read or write
+				if (traceCmd[1].equals("R")){
+					mmu.readMemory(page_number);
+					if (debug) {
+						System.out.println("reading      " + page_number);
+					}	
+				} else if (traceCmd[1].equals("W")){
+					mmu.writeMemory(page_number);
+					if (debug) {
+						System.out.println("writting     " + page_number);
+					}	
+				} else {
+						System.out.println("Badly formatted file. Error on line " + (numEvents+1));
+						System.exit(-1);
+				}
+            
+				//move to the next page
+				numEvents++;
+				traceLine = inputFile.readLine();
+			} //end of loop
+        
+		} catch (java.io.IOException e) {
+			System.out.println("Error reading inputFile file");
+			System.exit(-1);
+		} catch (NumberFormatException e) {
+			System.out.println("Memory address strange on line " + (numEvents+1));
+			System.exit(-1);
+		}
+   	}
+   	
+   	private static void DisplayResults() {
+   	    System.out.println("total memory frames:  " + frames);
+   	    System.out.println("events in trace:      " + numEvents);
+   	    System.out.println("total disk reads:     " + numDiskReads );
+   	    System.out.println("total disk writes:    " + numDiskWrites);
+   		java.text.DecimalFormat f = new java.text.DecimalFormat("0.0000");
+   		System.out.println("page fault rate: " + f.format(((double)numDiskReads)/numEvents));
+
+   	}
 
 }
